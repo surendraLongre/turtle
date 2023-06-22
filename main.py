06454cc2f78f9bd361f251c09eb50ffa07d6a634
surendraLongre <murari.longre2366@gmail.com>
2023-06-23 01:18:18 +0530

    finish drawing of spirograph

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ move=[30, -30]
 angle=[0,90,180,270]
 
 turtle.speed("fastest")
-turtle.width(10)
-for i in range(200):
-    turtle.color((random_color(),random_color(),random_color()))
-    turtle.forward(random.choice(move))
-    turtle.setheading(random.choice(angle))
+turtle.width(0)
+angle_change_by=5
+#draw spirograph
+for i in range(int(360/angle_change_by)):
+    turtle.circle(200)
+    turtle.right(angle_change_by)
 
+
+#spirograph drawing ends
 screen=Screen()
 screen.exitonclick()
