03aa376527dae86b47c195c4b91833ac46fe2d0c
surendraLongre <murari.longre2366@gmail.com>
2023-06-23 04:39:47 +0530

    finish turtle drawing based on user keypresses w s d a

--- main.py
+++ main.py
@@ -6,44 +6,50 @@ import random
 # write random color function code
 def random_color():
     return random.randint(0,255)
+def move_forward():
+    turtle.forward(10)
+
+def move_backward():
+    turtle.forward(-10)
+
+def left_angle():
+    turtle.left(10)
+
+def right_angle():
+    turtle.right(10)
+
+def right_curve():
+    right_angle()
+    move_forward()
+
+def left_curve():
+    left_angle()
+    move_backward()
+def clear():
+    turtle.clear()
+    turtle.penup()
+    turtle.home()
+    turtle.pendown()
+
 
 turtle=t.Turtle()
 t.colormode(255)
 turtle.shape('turtle')
+screen=Screen()
 #move=[30, -30]
 #angle=[0,90,180,270]
 #
 #turtle.speed("fastest")
 #turtle.width(3)
 #angle_change_by=5
-#draw hirst painting
-#get colors
-rgb_colors=[]
-colors=colorgram.extract('image.jpg', 30)
-for color in colors:
-    r=color.rgb.r
-    g=color.rgb.g
-    b=color.rgb.b
-    if (r<240 or g<240 or b<240):
-        rgb_colors.append((r,g,b))
-#start drawing hirst painting
-turtle.penup()
-turtle.setheading(230)
-turtle.forward(400)
-turtle.setheading(0)
-for i in range(15):
-    for j in range(10):
-        turtle.color(random.choice(rgb_colors))
-        turtle.pendown()
-        turtle.dot(20)
-        turtle.penup()
-        turtle.forward(50)
-    turtle.backward(8*50+10*20//2)
-    turtle.left(90)
-    turtle.forward(50)
-    turtle.right(90)
+#add key listeners
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=left_angle)
+screen.onkey(key="d", fun=right_angle)
+screen.onkey(key="c", fun=clear)
 
-#hirst painting ends
 print("done")
 screen=Screen()
 screen.exitonclick()
