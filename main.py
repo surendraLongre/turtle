13bba48db8d9944606c9b23af113b3b8b3cd6936
surendraLongre <murari.longre2366@gmail.com>
2023-06-22 23:46:53 +0530

    choose a random color instead of from a list

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from turtle import Turtle, Screen
+import turtle as t
+from turtle import Screen
 import random
 
-turtle=Turtle()
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+# write random color function code
+def random_color():
+    return random.randint(0,255)
+
+turtle=t.Turtle()
+t.colormode(255)
 turtle.shape('turtle')
 move=[30, -30]
 angle=[0,90,180,270]
@@ -10,7 +15,7 @@ angle=[0,90,180,270]
 turtle.speed("fastest")
 turtle.width(10)
 for i in range(200):
-    turtle.color(random.choice(colors))
+    turtle.color((random_color(),random_color(),random_color()))
     turtle.forward(random.choice(move))
     turtle.setheading(random.choice(angle))
 
